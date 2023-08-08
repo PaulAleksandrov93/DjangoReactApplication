ba28d5c364c88ceef51bccfe5f2ef913fcc66e1d
PaulAleksandrov93 <aleksandrov.pavel93@gmail.com>
2023-08-08 21:20:43 +0500

    Styling app

--- frontend/src/pages/NotePage.js
+++ frontend/src/pages/NotePage.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import { ReactComponent as ArrowLeft } from '../assets/arrow-left.svg'
 
 const NotePage = () => {
     const { id } = useParams();
@@ -17,8 +18,15 @@ const NotePage = () => {
     }, [getNote]);
 
     return (
-        <div>
-            <p>{note?.body}</p>
+        <div className='note'>
+            <div className='note-header'>
+                <h3>
+                    <Link to="/">
+                        <ArrowLeft />
+                    </Link>
+                </h3>
+            </div>
+            <textarea defaultValue={note?.body}></textarea>
         </div>
     );
 };
